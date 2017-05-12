7e50bf7004c572b6fbc571478ca295835b6953bf
Haipeng Fan <hafan@microsoft.com>
2017-05-12 11:02:34 -0700

    Implemented shorted path algorithm

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class Board extends Component {
       <div className="board" >
         {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((j) => {
           let row = [0, 1, 2, 3, 4, 5, 6, 7, 8].map((i) => {
-            return <Square key={i}/>;
+            return <Square key={i} />;
           });
           return <div className="board-row" key={j}>{row}</div>;
         })}
@@ -44,22 +44,22 @@ class StatusPanel extends Component {
         <div className="next-square">
           <span className="next-label">Next</span>
           {[0, 1, 2].map((i) => {
-            return <Square key={i}/>;
+            return <Square key={i} />;
           })}
         </div>
-        <div className="clear"/>
+        <div className="clear" />
       </div>
     );
   }
 }
 
 class Score extends Component {
-  render(){
+  render() {
     return (
       <div className="score">
         <span className="score-label">Score: </span>
         <span className="score-value">0</span>
-       </div>
+      </div>
     );
   }
 }
@@ -72,3 +72,93 @@ class InformationPanel extends Component {
 
 
 export default App;
+
+function findPath(squareMap, start, end) {
+  const ROW_NUMBER = 9;
+  if (!squareMap || squareMap.length != ROW_NUMBER * ROW_NUMBER || !start || !end) {
+    return null;
+  }
+
+  let currenNode = null;
+  var mapArray = squareMap.map((node, index) => {
+    let output = {
+      x = node.x,
+      y = node.y,
+      filled = node.filled,
+      visited = false,
+      distance = Infinity,
+      prev = null,
+    };
+
+    if (start.x == node.x && start.y == node.y) {
+      currentNode = output;
+      distance = 0;
+    }
+  });
+
+  if (!currentNode) {
+    return null;
+  }
+
+  let hasMore = true;
+  while (hasMore) {
+    curentNode.visited = true;
+    if (currentNode.x == end.x && currentNode.y == end.y) {
+      break;
+    }
+
+    if (currentNode.x > 0 &&
+      !mapArray[currentNode.y][currentNode.x - 1].filled &&
+      mapArray[currentNode.y][currentNode.x - 1].distance > currentNode.distance + 1) {
+      mapArray[cucrrentNode.y][currentNode.x - 1].distance = currentNode.distance + 1;
+      mapArray[cucrrentNode.y][currentNode.x - 1].prev = currentNode;
+    } 
+    
+    if (currentNode.x < ROW_NUMBER &&
+      !mapArray[currentNode.y][currentNode.x + 1].filled &&
+      mapArray[currentNode.y][currentNode.x + 1].distance > currentNode.distance + 1) {
+      mapArray[cucrrentNode.y][currentNode.x + 1].distance = currentNode.distance + 1;
+      mapArray[cucrrentNode.y][currentNode.x + 1].prev = currentNode;
+    } 
+    
+    if (currentNode.y > 0 &&
+      !mapArray[currentNode.y - 1][currentNode.x].filled &&
+      mapArray[currentNode.y - 1][currentNode.x].distance > currentNode.distance + 1) {
+      mapArray[cucrrentNode.y - 1][currentNode.x].distance = currentNode.distance + 1;
+      mapArray[cucrrentNode.y - 1][currentNode.x].prev = currentNode;
+    } 
+    
+    if (currentNode.y < ROW_NUMBER &&
+      !mapArray[currentNode.y + 1][currentNode.x].filled &&
+      mapArray[currentNode.y + 1][currentNode.x].distance > currentNode.distance + 1) {
+      mapArray[cucrrentNode.y + 1][currentNode.x].distance = currentNode.distance + 1;
+      mapArray[cucrrentNode.y + 1][currentNode.x].prev = currentNode;
+    }
+
+    let shortestDist = Infinity;
+    hasMore = false;
+    for (let i = 0; i < mapArray.length; i++) {
+      if (mapArray[i].filled || mapArray[i].visited) {
+        continue;
+      }
+
+      if (mapArray[i].distance < shortestDist) {
+        shortestDist = mapArray[i].distance;
+        currentNode = mapArray[i];
+        hasMore = true;
+      }
+    }
+
+    if (currentNode.x == end.x && currentNode.y == end.y) {
+      var path = [];
+      while (currentNode.prev) {
+        path.unshift(currentNode);
+        currentNode = currentNode.prev;
+      }
+
+      return path;
+    }
+
+    return null;
+  }
+}
