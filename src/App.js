05e4a85ee8bd77b7786b585704fceca69e09415b
Haipeng Fan <hafan@microsoft.com>
2017-05-12 18:10:43 -0700

    wire components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,75 @@
 import React, { Component } from 'react';
 import './App.css';
 
+var NODE_TYPES = 6
+var ROW_NUMBER = 9;
+var NEW_NODE_NUMBER = 3;
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      squares: [],
+      nextSquares: [],
+      playing: false
+    };
+  }
+
+  startGame() {
+    let initialSquares = this.setupSquares();
+    let initialNextSquares = this.getNextSquares();
+    this.setState({
+      squares: initialSquares,
+      nextSquares: initialNextSquares,
+      playing: true
+    });
+  }
+
+  setupSquares() {
+    let squares = new Array(ROW_NUMBER * ROW_NUMBER);
+    for (let i = 0; i < squares.length; i++) {
+      squares[i] = {
+        x: i,
+        y: i % ROW_NUMBER,
+        filled: false,
+        type: null
+      }
+    }
+
+    this.addNewNodes(squares);
+    return squares;
+  }
+
+  addNewNodes(squares) {
+    let nextSquares = this.getNextSquares();
+    for (let i = 0; i < nextSquares.length; i++) {
+      var nextNodeIndex = Math.floor(Math.random() * ROW_NUMBER * ROW_NUMBER);
+      do {
+        if (!squares[nextNodeIndex].filled) {
+          squares[nextNodeIndex].filled = true;
+          squares[nextNodeIndex].type = nextSquares[i];
+          break;
+        } else {
+          nextNodeIndex++;
+          if (nextNodeIndex >= ROW_NUMBER * ROW_NUMBER) {
+            nextNodeIndex = 0;
+          }
+        }
+      } while (!squares[nextNodeIndex].filled)
+    }
+  }
+
+  getNextSquares() {
+    let result = [];
+    for (let i = 0; i < NEW_NODE_NUMBER; i++) {
+      result.push(Math.floor(Math.random() * NODE_TYPES + 1));
+    }
+    return result;
+  }
+
   render() {
     return (
       <div className="app">
-        <InformationPanel />
+        <InformationPanel startButtonText={this.state.playing ? "Restart Game" : "Start Game"} onStartButtonClick={() => this.startGame()} />
         <Board />
         <StatusPanel />
       </div>
@@ -66,7 +130,11 @@ class Score extends Component {
 
 class InformationPanel extends Component {
   render() {
-    return <div className="info-panel" >Welcome!</div>;
+    return (
+      <div className="info-panel">
+        <span>Welcome!</span>
+        <button className="start-button" onClick={this.props.onStartButtonClick()}>{this.props.startButtonText}</button>
+      </div>);
   }
 }
 
@@ -74,26 +142,27 @@ class InformationPanel extends Component {
 export default App;
 
 function findPath(squareMap, start, end) {
-  const ROW_NUMBER = 9;
-  if (!squareMap || squareMap.length != ROW_NUMBER * ROW_NUMBER || !start || !end) {
+  if (!squareMap || squareMap.length !== ROW_NUMBER * ROW_NUMBER || !start || !end) {
     return null;
   }
 
-  let currenNode = null;
+  let currentNode = null;
   var mapArray = squareMap.map((node, index) => {
     let output = {
-      x = node.x,
-      y = node.y,
-      filled = node.filled,
-      visited = false,
-      distance = Infinity,
-      prev = null,
+      x: node.x,
+      y: node.y,
+      filled: node.filled,
+      visited: false,
+      distance: Infinity,
+      prev: null,
     };
 
-    if (start.x == node.x && start.y == node.y) {
+    if (start.x === node.x && start.y === node.y) {
       currentNode = output;
-      distance = 0;
+      currentNode.distance = 0;
     }
+
+    return output;
   });
 
   if (!currentNode) {
@@ -102,37 +171,37 @@ function findPath(squareMap, start, end) {
 
   let hasMore = true;
   while (hasMore) {
-    curentNode.visited = true;
-    if (currentNode.x == end.x && currentNode.y == end.y) {
+    currentNode.visited = true;
+    if (currentNode.x === end.x && currentNode.y === end.y) {
       break;
     }
 
     if (currentNode.x > 0 &&
       !mapArray[currentNode.y][currentNode.x - 1].filled &&
       mapArray[currentNode.y][currentNode.x - 1].distance > currentNode.distance + 1) {
-      mapArray[cucrrentNode.y][currentNode.x - 1].distance = currentNode.distance + 1;
-      mapArray[cucrrentNode.y][currentNode.x - 1].prev = currentNode;
-    } 
-    
+      mapArray[currentNode.y][currentNode.x - 1].distance = currentNode.distance + 1;
+      mapArray[currentNode.y][currentNode.x - 1].prev = currentNode;
+    }
+
     if (currentNode.x < ROW_NUMBER &&
       !mapArray[currentNode.y][currentNode.x + 1].filled &&
       mapArray[currentNode.y][currentNode.x + 1].distance > currentNode.distance + 1) {
-      mapArray[cucrrentNode.y][currentNode.x + 1].distance = currentNode.distance + 1;
-      mapArray[cucrrentNode.y][currentNode.x + 1].prev = currentNode;
-    } 
-    
+      mapArray[currentNode.y][currentNode.x + 1].distance = currentNode.distance + 1;
+      mapArray[currentNode.y][currentNode.x + 1].prev = currentNode;
+    }
+
     if (currentNode.y > 0 &&
       !mapArray[currentNode.y - 1][currentNode.x].filled &&
       mapArray[currentNode.y - 1][currentNode.x].distance > currentNode.distance + 1) {
-      mapArray[cucrrentNode.y - 1][currentNode.x].distance = currentNode.distance + 1;
-      mapArray[cucrrentNode.y - 1][currentNode.x].prev = currentNode;
-    } 
-    
+      mapArray[currentNode.y - 1][currentNode.x].distance = currentNode.distance + 1;
+      mapArray[currentNode.y - 1][currentNode.x].prev = currentNode;
+    }
+
     if (currentNode.y < ROW_NUMBER &&
       !mapArray[currentNode.y + 1][currentNode.x].filled &&
       mapArray[currentNode.y + 1][currentNode.x].distance > currentNode.distance + 1) {
-      mapArray[cucrrentNode.y + 1][currentNode.x].distance = currentNode.distance + 1;
-      mapArray[cucrrentNode.y + 1][currentNode.x].prev = currentNode;
+      mapArray[currentNode.y + 1][currentNode.x].distance = currentNode.distance + 1;
+      mapArray[currentNode.y + 1][currentNode.x].prev = currentNode;
     }
 
     let shortestDist = Infinity;
@@ -149,7 +218,7 @@ function findPath(squareMap, start, end) {
       }
     }
 
-    if (currentNode.x == end.x && currentNode.y == end.y) {
+    if (currentNode.x === end.x && currentNode.y === end.y) {
       var path = [];
       while (currentNode.prev) {
         path.unshift(currentNode);
